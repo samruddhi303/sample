755ef7134e5cc7ae985344fecee68c6000f34e4c
samruddhi303 <samruddhi1584@gmail.com>
2023-03-22 20:50:56 +0530

    Introduction

---
+++ Introduction to python.py
@@ -0,0 +1,5 @@
+#Introduction to python
+#python is highly expressive ,object oriented,interprted,scripting language. python is used in web devlopement, software devlopement,Ai/machine learning, GUI.
+#first program
+
+print("Hello world!!") #In python we use print fuction to give output.
