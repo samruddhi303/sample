6e5499f3057748174cdfd8624b5c4fa865d2d141
samruddhi303 <samruddhi1584@gmail.com>
2023-03-24 21:56:07 +0530

    commit on print

--- Introduction to python.py
+++ Introduction to python.py
@@ -2,4 +2,9 @@
 #python is highly expressive ,object oriented,interprted,scripting language. python is used in web devlopement, software devlopement,Ai/machine learning, GUI.
 #first program
 
-print("Hello world!!") #In python we use print fuction to give output.
+#In python we use print fuction to give output.
+print("Hello world!!") 
+
+
+#print contain fuction end which has default value "\n" print("hello world" end="/n")
+print("Hii \n Good Evening \n Welcome To Python Programming!!")
