ecbaab06acc54512c8c1abc5f1ac988d012142c2
samruddhi303 <samruddhi1584@gmail.com>
2023-03-26 23:18:49 +0530

    commit on datatypes

--- Datatypes.py
+++ Datatypes.py
@@ -1,2 +1,23 @@
-val1 = "Hello python"
-type('val1')
+# Datatypes in python programming include:
+# Numbers,String,List, Tuple, Set, Dictionary, Boolean, 
+
+# List: Lists are used to store multiple items in a single variable.
+students = ["sahil","Rahul","Ashwini"]
+print(students)
+
+# tuple: tuple are used to store multiple items in a single variable.
+myBasket = ("Apple","Banana","Orange","Grapes","Apple","Apple")
+print(myBasket)
+
+#set: It Data type which Act like Mathmatical sets. { }= Define sets.
+myColors = {"sky","red","pink","yellow","black"}
+print(myColors)
+
+#Dictionaries are used to store data values in key:value pairs.
+fruitsNameToImage = {
+    "Apple" : "🍎",
+    "Banana" : "🍌",
+    "cherry" : "🍒",
+    "Grapes" : "🍇",
+    "Orange" : "🍊"
+}
