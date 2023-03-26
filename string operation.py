5a35db13e9e292e89b71d1e2f83f96012b2a3d30
samruddhi303 <samruddhi1584@gmail.com>
2023-03-27 00:15:43 +0530

    inoperator

--- string operation.py
+++ string operation.py
@@ -3,3 +3,28 @@
 rate,quantity = input("enter the rate and quantity: ").split()
 Bill = int(rate) * int(quantity)
 print(Bill)
+
+#Multiline string-->
+#You can print multiples lines then we enclosing that lines it in a triple quotes """ """.
+poem="""
+Twinkle, twinkle, little star,
+How I wonder what you are.
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are!"""
+print(poem)
+
+#Slicing-->
+#A slice object is used to specify how to slice a sequence. You can specify where to start the slicing, and where to end.
+str1 = "mango is sweet"
+print(str1[2:7])
+
+#Length function
+#len() is a built-in function in python. You can use the len() to get the length of the given string, array, list
+name = "RadhaKrishna"
+print(len(name))
+
+# in operator
+#The 'in' operator is used to check if a value exists in a sequence or not.
+print("Radha" in name)
