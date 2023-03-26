d96defdaddb8e4cedda8cba427e9357b6b2e4716
samruddhi303 <samruddhi1584@gmail.com>
2023-03-26 23:27:15 +0530

    commits

--- Datatypes.py
+++ Datatypes.py
@@ -1,6 +1,14 @@
 # Datatypes in python programming include:
 # Numbers,String,List, Tuple, Set, Dictionary, Boolean, 
 
+#Numbers: used to store real values
+num1 = 100
+print(num1)
+
+#String: string are used to store charater type value
+Name = "samruddhi"
+print("Name = ",Name)
+
 # List: Lists are used to store multiple items in a single variable.
 students = ["sahil","Rahul","Ashwini"]
 print(students)
@@ -14,10 +22,11 @@ myColors = {"sky","red","pink","yellow","black"}
 print(myColors)
 
 #Dictionaries are used to store data values in key:value pairs.
-fruitsNameToImage = {
-    "Apple" : "🍎",
-    "Banana" : "🍌",
-    "cherry" : "🍒",
-    "Grapes" : "🍇",
-    "Orange" : "🍊"
+
+Students = {
+   " smita" : "12",
+   " arya" : "29",
+   " prachi": "31",
+   " kiara": "86"
 }
+print(Students)
