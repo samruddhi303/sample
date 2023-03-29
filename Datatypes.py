1a814438d5efe7a084608c819934416d667cd7cd
samruddhi303 <samruddhi1584@gmail.com>
2023-03-29 23:51:50 +0530

    datatypes

--- Datatypes.py
+++ Datatypes.py
@@ -5,24 +5,28 @@
 num1 = 100
 print(num1)
 
+
 #String: string are used to store charater type value
 Name = "samruddhi"
 print("Name = ",Name)
+
 
 # List: Lists are used to store multiple items in a single variable.
 students = ["sahil","Rahul","Ashwini"]
 print(students)
 
+
 # tuple: tuple are used to store multiple items in a single variable.
 myBasket = ("Apple","Banana","Orange","Grapes","Apple","Apple")
 print(myBasket)
+
 
 #set: It Data type which Act like Mathmatical sets. { }= Define sets.
 myColors = {"sky","red","pink","yellow","black"}
 print(myColors)
 
-#Dictionaries are used to store data values in key:value pairs.
 
+#Dictionaries are used to store data values in key:value pairs.
 Students = {
    " smita" : "12",
    " arya" : "29",
--- variable.py
+++ variable.py
@@ -5,12 +5,9 @@ print("Name = ", Name)
 #another Ex
 Name = "sam"
 print("Name =",Name)
-type(Name)
 Age = 20
 print("Age = ", Age)
 weight = 45
 print("weight = ",weight)
 
 
-#Datatype  : In python we use type() function to find the Datatype of variable
-type('hello python')
