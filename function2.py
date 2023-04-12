37459da9575cd77050a25c773d89eaee3261eb23
samruddhi303 <samruddhi1584@gmail.com>
2023-04-12 19:54:19 +0530

    function

--- function2.py
+++ function2.py
@@ -1,8 +1,19 @@
-num = int(input("How many times do you want to print: "))
-f = 0
-s = 1
-for i in range(num):
- num = f + s
- f = s
- s = num
-print(num)
+def cal_geomean(a, b): #Declaring a function
+    mean = (a*b)/(a+b)  #Defining a function
+    print(mean)  
+def Is_greater(a, b):
+    if(a>b):
+        print("a is greater!")
+    else:
+        print("b is greater!")
+
+
+a = 9
+b = 2
+cal_geomean(a, b) #calling the function
+Is_greater(a, b)
+
+b = 128
+a = 90
+cal_geomean(a, b) #calling the function
+Is_greater(a, b)
