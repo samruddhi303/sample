42836029bd15acba101d354fa3aeaba5193009ab
samruddhi303 <samruddhi1584@gmail.com>
2023-04-27 13:24:58 +0530

    commit-finally

--- FinallyKeyword.py
+++ FinallyKeyword.py
@@ -1,3 +1,5 @@
+# Finally keyword -->The finally keyword is available in Python, and it is always used after the try-except block.
+# The finally code block is always executed after the try block has terminated normally or after the try block has terminated for some other reason
 def func1():
     try:
         l = [5,3,9,8,7]
