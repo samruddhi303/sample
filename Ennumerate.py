e9c12d7ca470d8941fbbe66a820a54255777121a
samruddhi303 <samruddhi1584@gmail.com>
2023-04-30 12:09:11 +0530

    Enumerate.py

---
+++ Ennumerate.py
@@ -0,0 +1,15 @@
+# enumerate--. Inbuilt function in python that allow you to keep tracks of the number of iteration (loops)in loop
+marks = [12,66,32,43,54,68,86]
+for index,mark in enumerate(marks):
+    print(mark)
+    if(index==3):
+        print("sam awesome")
+    index =+1
+
+    
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if(index == 3):
+#         print("harry hi")
+#     index +=1
